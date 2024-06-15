4fb5c7793c0b75018f77a962d2c715acde2b8e02
kymzzang6 <139032375+kymzzang6@users.noreply.github.com>
2024-06-15 14:07:36 +0900

    Update sketch_may30b.ino

--- sketch_may30b.ino
+++ sketch_may30b.ino
@@ -1,136 +1,6 @@
 #include <Stepper.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-const int stepsPerRevolution = 1024;
-Stepper Step1(stepsPerRevolution, 11, 9, 10, 8);
-Stepper Step2(stepsPerRevolution, 7, 5, 6, 4);
-
-#define LED1 A1
-#define LED2 A0
-#define btn1 12
-#define btn2 13
-
-
-bool finish = false;
-char data;
-
-
-void setup() {
-  Step1.setSpeed(25);
-  Step2.setSpeed(25);
-  Serial.begin(9600);
-
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(btn1, INPUT_PULLUP);
-  pinMode(btn2, INPUT_PULLUP);
-
-
-  lcd.init();
-  lcd.backlight();
-  if (Serial.available()) {
-    if (Serial.read() == 'S') {
-      Serial.println("O");
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, LOW);
-      lcd.setCursor(0, 0);
-      lcd.print("Recognizing");
-    }
-
-  }
-
-
-}
-
-void loop() {
-  if (Serial.available()) {
-
-    if (finish == false) {
-      data = Serial.read();
-      lcd.init();
-      lcd.setCursor(0, 0);
-      lcd.print("Recognizing");
-    }
-    else {
-      data = 'X';
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, LOW);
-      Step1.step(0);
-      Step2.step(0);
-      lcd.init();
-      lcd.setCursor(0, 0);
-      lcd.print("Recognized");
-      
-      if (digitalRead(btn1) == LOW) {
-        Serial.println("1번 잠김");
-        Step1.step(-stepsPerRevolution / 2);
-        digitalWrite(LED1, LOW);
-        lcd.init();
-        lcd.setCursor(0, 0);
-        lcd.print("Close");
-        
-      }
-      else if (digitalRead(btn2) == LOW) {
-        Serial.println("2번 잠김");
-        Step2.step(-stepsPerRevolution / 2);
-        digitalWrite(LED2, LOW);
-        lcd.init();
-        lcd.setCursor(0, 0);
-        lcd.print("Close");
-        
-      }
-    }
-
-
-
-
-    switch (data) {
-      case '0':
-        lcd.init();
-        lcd.setCursor(0, 0);
-        lcd.print("Unknown");
-        delay(200);
-        break;
-
-      case '1':
-        Serial.println("1번");
-        Step1.step(stepsPerRevolution / 2);
-        digitalWrite(LED1, HIGH);
-        lcd.init();
-        lcd.setCursor(0, 0);
-        lcd.print("1_Open");
-        delay(1000);
-        finish = true;
-        break;
-
-      case '2':
-        Serial.println("2번");
-        Step2.step(512);
-        digitalWrite(LED2, HIGH);
-        lcd.init();
-        lcd.setCursor(0, 0);
-        lcd.print("2_Open");
-        delay(1000);
-        finish = true;
-        break;
-      default:
-        break;
-    }
-
-
-
-  }
-
-
-}
-
-
-#include <Stepper.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd (0x27, 16, 2);
 
 const int stepsPerRevolution = 1024;
@@ -145,7 +15,7 @@ Stepper Step2(stepsPerRevolution, 7, 5, 6, 4);
 void setup() {
   Step1.setSpeed(60);
   Step2.setSpeed(60);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -184,7 +54,7 @@ void Sa() {
   digitalWrite(LED1, LOW);
   digitalWrite(LED2, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("얼굴을 카메라에 보여주세요");
+  lcd.print("recognizing");
   Step1.step(0);
   Step2.step(0);
 }
@@ -193,7 +63,7 @@ void Sb() {
   Step1.step(512);
   digitalWrite(LED1, HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("1번 사물함 잠금해제");
+  lcd.print("open_1");
   delay(200);    
 }
 
@@ -201,7 +71,7 @@ void Sc() {
   Step2.step(512);
   digitalWrite(LED2, HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("2번 사물함 잠금해제");
+  lcd.print("open_2");
   delay(200);
 }
 
@@ -209,7 +79,7 @@ void Ra() {
   Step1.step(-512);
   digitalWrite(LED1, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("1번 사물함이 잠겼습니다");
+  lcd.print("close_1");
   int btn1 = 0;
   delay(200);
 }
@@ -218,10 +88,7 @@ void Rb() {
   Step2.step(-512);
   digitalWrite(LED2, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("2번 사물함이 잠겼습니다");
+  lcd.print("close2");
   int btn2 = 0;
   delay(200); 
 }
-
-
-
