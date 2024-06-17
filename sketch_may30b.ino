18ee3ee903f98758350f191c723b842b16c3c02b
LIM0909 <150766693+lim0303003@users.noreply.github.com>
2024-06-17 15:31:21 +0900

    Update sketch_may30b.ino

--- sketch_may30b.ino
+++ sketch_may30b.ino
@@ -1,8 +1,7 @@
 #include <Stepper.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd (0x27, 16, 2);
 
 const int stepsPerRevolution = 1024;
 Stepper Step1(stepsPerRevolution, 11, 9, 10, 8);
@@ -11,116 +10,77 @@ Stepper Step2(stepsPerRevolution, 7, 5, 6, 4);
 #define button1 12
 #define button2 13
 
-int btn1;
-int btn2;
-
-bool finish = false;
-char data;
+int btn1
+int btn2
 
 void setup() {
-  Step1.setSpeed(25);
-  Step2.setSpeed(25);
+  Step1.setSpeed(60);
+  Step2.setSpeed(60);
   Serial.begin(9600);
-
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-
   lcd.init();
   lcd.backlight();
-  resetSystem();
 }
 
 void loop() {
-  if (Serial.available()) {
-    if (finish == false) {
-      data = Serial.read();
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Recognizing");
-      processSerialData();
+    if (Serial.available()) {      
+      char data = Serial.readString();
+    if (data == 0 && btn1 == 0 && btn2 == 0) {
+      Sa();
+    }
+    else if (data == 1) {
+      Sb();    
+    }
+    else if (data == 2) {
+      Sc();
+    }
+    else if (btn1 == 1) {
+      Ra();
+    }
+    else if (btn2 == 1) {
+      Rb();   
+    }
+    if (button1 == HIGH) {
+      btn1 = 1; 
+    }
+    if (button2 == HIGH) {
+      btn2 = 1;
     }
   }
-  
-  if (button1 == HIGH) {
-    btn1 == 1;
-  }
-
-  if (button2 == HIGH) {
-    btn2 == 1;
-  }
-
-  if (finish == true) {
-    checkButtons();
-  }
 }
 
-void processSerialData() {
-  switch (data) {
-    case '0':
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Unknown");
-      delay(200);
-      break;
-      
-    case '1':
-      Step1.step(stepsPerRevolution / 2);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("1_Open");
-      delay(1000);
-      finish = true;
-      break;
-
-    case '2':
-      Step2.step(stepsPerRevolution / 2);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("2_Open");
-      delay(1000);
-      finish = true;
-      break;
-      
-    default:
-      break;
-  }
-}
-
-void checkButtons() {
-  if (btn1 == 1) {
-    Step1.step(-stepsPerRevolution / 2);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("1_Close");
-    btn1 = 0;
-    delay(1000);
-    resetSystem();
-  }
-  else if (btn2 == 1) {
-    Step2.step(-stepsPerRevolution / 2);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("2_Close");
-    btn2 = 0;
-    delay(1000);
-    resetSystem();
-  }
-}
-
-void resetSystem() {
-  finish = false;
-  lcd.clear();
+void Sa() {
   lcd.setCursor(0, 0);
-  lcd.print("Waiting for S");
+  lcd.print("See Your face on screen");
+  Step1.step(0);
+  Step2.step(0);
+}
 
-  // 'S' 신호를 받을 때까지 대기
-  while (!Serial.available()) {
-    // 대기 루프
-  }
+void Sb() {
+  Step1.step(512);
+  lcd.setCursor(0, 0);
+  lcd.print("no.1 rocker open");
+  delay(200);    
+}
 
-  if (Serial.read() == 'S') {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Recognizing");
-  }
+void Sc() { 
+  Step2.step(512);
+  lcd.setCursor(0, 0);
+  lcd.print("no.2 rocker open");
+  delay(200);
+}
+
+void Ra() {
+  Step1.step(-512);
+  lcd.setCursor(0, 0);
+  lcd.print("no.1 rocker rock");
+  btn1 -= 1;
+  delay(200);
+}
+
+void Rb() {
+  Step2.step(-512);
+  lcd.setCursor(0, 0);
+  lcd.print("no.2 rocker rock");
+  btn2 -= 1;
+  delay(200);
 }
