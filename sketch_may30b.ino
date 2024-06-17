ed10420b0bf7fc71ef70239dc7b3293f5121cfc3
LIM0909 <150766693+lim0303003@users.noreply.github.com>
2024-06-17 13:35:23 +0900

    Update sketch_may30b.ino

--- sketch_may30b.ino
+++ sketch_may30b.ino
@@ -7,8 +7,6 @@ const int stepsPerRevolution = 1024;
 Stepper Step1(stepsPerRevolution, 11, 9, 10, 8);
 Stepper Step2(stepsPerRevolution, 7, 5, 6, 4);
 
-#define LED1 2
-#define LED2 3
 #define button1 12
 #define button2 13
 
@@ -16,10 +14,6 @@ void setup() {
   Step1.setSpeed(60);
   Step2.setSpeed(60);
   Serial.begin(9600);
-
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-
   lcd.init();
   lcd.backlight();
 }
@@ -51,44 +45,38 @@ void loop() {
 }
 
 void Sa() {
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("recognizing");
+  lcd.print("See Your face on screen");
   Step1.step(0);
   Step2.step(0);
 }
 
 void Sb() {
   Step1.step(512);
-  digitalWrite(LED1, HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("open_1");
+  lcd.print("no.1 rocker open");
   delay(200);    
 }
 
 void Sc() { 
   Step2.step(512);
-  digitalWrite(LED2, HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("open_2");
+  lcd.print("no.2 rocker open");
   delay(200);
 }
 
 void Ra() {
   Step1.step(-512);
-  digitalWrite(LED1, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("close_1");
+  lcd.print("no.1 rocker rock");
   int btn1 = 0;
   delay(200);
 }
 
 void Rb() {
   Step2.step(-512);
-  digitalWrite(LED2, LOW);
   lcd.setCursor(0, 0);
-  lcd.print("close2");
+  lcd.print("no.2 rocker rock");
   int btn2 = 0;
   delay(200); 
 }
